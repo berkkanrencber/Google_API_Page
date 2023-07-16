a18eb6dd17da8c67ef5754833635c2d413d3ea52
berkkanrencber <101739059+berkkanrencber@users.noreply.github.com>
2023-07-16 13:49:23 +0300

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -7,7 +7,7 @@ inputBox.addEventListener('input', onChangeLocation);
 
 function onChangeLocation(){
   var location = document.getElementById('autocomplete').value;
-  let URL = "http://localhost:8080/get/autocomplete?input=" + location;
+  let URL = "https://google-api-vercel-berkkanrencber.vercel.app/get/autocomplete?input=" + location;
 
   sendRequest(URL, 'GET')
         .then(data => {   
